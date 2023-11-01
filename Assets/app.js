9e20e3b93048c09e9fc026374c6462d26f612354
Chrstjan <ps5christian@hotmail.com>
2023-11-01 09:02:34 +0100

    Started making the js validation

--- Assets/app.js
+++ Assets/app.js
@@ -1,4 +1,4 @@
-
+//Play the PUMPKIN! sound when clicking on the pumpkins
 function playAudio() {
     let audio = document.getElementById('pumpkinSound');
 
@@ -10,3 +10,23 @@ function playAudio() {
         audio.currentTime = 0;
     }
 }
+
+//Form javaScript validation
+const fullName = document.getElementById('fName');
+const telNumber = document.getElementById('telNumber');
+const email = document.getElementById('email');
+
+const form = document.getElementById('sign-up-form');
+const errorMessage = document.getElementById('errorMessage');
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if (name.value === "" || name.value === null) {
+        messages.push("Navn skal udfyldes");
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorMessage.innerText = messages.join(", ");
+    }
+})
